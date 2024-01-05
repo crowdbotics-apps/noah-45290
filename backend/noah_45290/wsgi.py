fa42a81ae9d4116e314bd0915a7fee6a6ac0fbd8
Crowdbotics <team@crowdbotics.com>
2024-01-05 10:13:01 +0000

    Initial commit

---
+++ backend/noah_45290/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for noah_45290 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "noah_45290.settings"
+)
+
+application = get_wsgi_application()
